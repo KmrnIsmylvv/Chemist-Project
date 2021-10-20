17a9994e14344d66cf1a7d18a21608aea6a72277
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-10-21 03:30:24 +0400

    feat: added DataAcces layer and DbContext class

---
+++ DataAccess/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public static class DbContext
+    {
+        public static List<Medicine> Medicines { get;  }
+        public static List<MedicineType> MedicineTypes { get; }
+
+        static DbContext()
+        {
+            Medicines = new List<Medicine>();
+            MedicineTypes = new List<MedicineType>();
+        }
+    }
+}
