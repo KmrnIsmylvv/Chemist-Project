894bc23730bb24dab54aac5a8fb0ef20ebaedf18
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-10-22 14:16:09 +0400

    feat: added MedicineService class

---
+++ Business/Services/MedicineService.cs
@@ -0,0 +1,46 @@
+﻿using Business.Interfaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Services
+{
+    public class MedicineService : IMedicine
+    {
+        public Medicine Create(Medicine entity, string MedicineTypeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medicine> GetAll(string medicineTypeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medicine> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Update(Medicine entity, string MedicineTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
